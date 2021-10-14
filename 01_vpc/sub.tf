b4a071fa521c438de80a16aeb07b9ca8d8751650
unknown <amkk0425@gmail.com>
2021-10-14 17:33:04 +0900

    10.14

---
+++ 01_vpc/sub.tf
@@ -0,0 +1,32 @@
+resource "aws_subnet" "sub-puba"{
+    vpc_id  = aws_vpc.tf-vpc.id
+    cidr_block = "10.0.0.0/24"
+
+    tags = {
+        Name = "sub-pub-a"
+    }
+}
+resource "aws_subnet" "sub-pria"{
+    vpc_id  = aws_vpc.tf-vpc.id
+    cidr_block = "10.0.1.0/24"
+
+    tags = {
+        Name = "sub-pri-a"
+    }
+}
+resource "aws_subnet" "sub-prib"{
+    vpc_id  = aws_vpc.tf-vpc.id
+    cidr_block = "10.0.2.0/24"
+
+    tags = {
+        Name = "sub-pri-b"
+    }
+}
+resource "aws_subnet" "sub-pric"{
+    vpc_id  = aws_vpc.tf-vpc.id
+    cidr_block = "10.0.3.0/24"
+
+    tags = {
+        Name = "sub-pri-c"
+    }
+}
